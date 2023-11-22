b60fa703b60bbd88bd057114111611f1b7f59dc4
Alexis JAPAS <alexis.japas.dev@pm.me>
2023-11-22 18:07:57 +0100

    Positions init: secure

--- src/Lab.py
+++ src/Lab.py
@@ -1,40 +1,57 @@
 import numpy as np
-from random import shuffle
+from random import shuffle, randint
 
 from Agent import Agent
 
 
 class Lab:
-    def __init__(self, height: int, width: int, initial_population: int):
+    # INITIALIZATION
+    def __init__(self, height: int, width: int, init_population_count: int):
+        assert init_population_count <= height * width
+
         # map
         self.height = height
         self.width = width
+        self.population_map = np.full(
+            shape=(height, width), fill_value=None, dtype=object
+        )
 
         # population
-        y_pos = [y * height // initial_population for y in range(0, initial_population)]
-        shuffle(y_pos)
-        x_pos = [x * width // initial_population for x in range(0, initial_population)]
-        shuffle(x_pos)
-        self.population = [
-            Agent((y_pos.pop(), x_pos.pop()), 0) for _ in range(initial_population)
-        ]
+        self.population = self._init_population(init_population_count)
 
-        # timeline metrics
-        initial_population_map = self.get_population_map()
-        self.population_maps = initial_population_map[np.newaxis, :, :]
+    def _init_population(
+        self, init_population_count: int, distribution: str = "random"
+    ) -> list[Agent]:
+        positions = []
+        match distribution:
+            case "random":
+                while len(positions) < init_population_count:
+                    new_pos = (randint(0, self.height - 1), randint(0, self.width - 1))
+                    if new_pos not in positions:
+                        positions.append(new_pos)
+        return [Agent(self.population_map, pos, 0) for pos in positions]
 
-    def get_population_map(self):
-        current_map = np.zeros((self.height, self.width))
-        for agent in self.population:
-            current_map[agent.position] = agent.id
-        
-        return current_map
+    # VISUALIZATION
+    def get_id_map(self):
+        id_map = np.vectorize(
+            lambda agent: 255 - agent.id % 200 if agent is not None else 0,
+        )(self.population_map)
+        return id_map
 
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
 
-    lab = Lab(height=10, width=20, initial_population=200)
-    print(np.sum(lab.population_maps[-1] != 0))
-    plt.imshow(lab.population_maps[-1])
+    # Settings
+    height = 36
+    width = 50
+    init_pop_count = 100
+
+    # Init lab
+    lab = Lab(height=height, width=width, init_population_count=init_pop_count)
+    assert np.sum(lab.population_map != None) == init_pop_count
+
+    # Visualization
+    id_map = lab.get_id_map()
+    plt.imshow(id_map)
     plt.show()
