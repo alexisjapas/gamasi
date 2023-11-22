942f396160dea0e43b7c5fae6181e525628b32a3
Alexis JAPAS <alexis.japas.dev@pm.me>
2023-11-22 16:29:20 +0100

    New __repr__

--- src/Agent.py
+++ src/Agent.py
@@ -1,3 +1,4 @@
+import numpy as np
 from random import randint
 
 
@@ -5,35 +6,32 @@ class Agent:
     current_count = 0
     total_count = 0
 
-    def __init__(self, initial_position: (int, int), generation: int):
+    def __init__(
+        self, population_map: np.array, initial_position: (int, int), generation: int
+    ):
+        # Agent properties
         self.position = initial_position
         self.generation = generation
-
         self.age = 0
-
-        # id
         self.id = Agent.total_count
+
+        # Map
+        self.population_map = population_map
+        self.population_map[initial_position] = self
+
+        # Increment agents counters
         Agent.current_count += 1
         Agent.total_count += 1
 
+    def __del__(self):
+        Agent.current_count -= 1
+
     def __repr__(self):
+        return f"{self.id}"
+
+    def __str__(self):
         ID = f"ID: {self.id:05d}"
         GEN = f"GEN: {self.generation:05d}"
         AGE = f"AGE: {self.age:05}"
         POS = f"POS: {self.position}"
         return f"{ID} | {GEN} | {AGE} | {POS}\n"
-
-    def __del__(self):
-        Agent.current_count -= 1
-
-
-if __name__ == "__main__":
-    agents = [Agent(0) for i in range(10)]
-    a = Agent((0, 0), 0)
-    b = Agent((0, 1), 0)
-    c = Agent(1)
-    print(Agent.current_count)
-    del b
-    print(Agent.current_count)
-    agents.pop()
-    print(Agent.current_count)
