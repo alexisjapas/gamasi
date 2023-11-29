ba05b2dea83e778b640a20b893ab603b392e72d6
Alexis JAPAS <alexis.japas.dev@pm.me>
2023-11-29 17:24:14 +0100

    Experiment & analyze methods

--- src/Lab.py
+++ src/Lab.py
@@ -4,23 +4,39 @@ from random import shuffle, randint
 from time import sleep
 
 from Agent import Agent
+from Space import Space
+from Position import Position
 
 
 class Lab:
-    # INITIALIZATION
     def __init__(self, height: int, width: int, init_population_count: int):
         assert init_population_count <= height * width
 
-        # map
-        self.height = height
-        self.width = width
-        self.population_map = np.full(
-            shape=(height, width), fill_value=None, dtype=object
-        )
-        self.map_lock = threading.Lock()
+        # Spacetime
+        self.space = Space(height=height, width=width, lock=threading.Lock())
 
-        # population
+        # Population
         self.population = self._invoke_population(init_population_count)
+
+    def experiment(self, duration):
+        # Start
+        for agent in lab.population:
+            agent.start()
+        sleep(duration)
+
+        # Stop
+        for agent in lab.population:
+            agent.kill()
+
+    def analyze(self, n_viz):
+        assert n_viz <= len(self.population)
+        n_viz = 3
+        for i in range(n_viz):
+            plt.figure(f"Agent's n°{i} path")
+            plt.imshow(lab.population[i].array_path)
+        plt.figure("Final positions")
+        plt.imshow(lab.space.displayable)
+        plt.show()
 
     def _invoke_population(
         self, init_population_count: int, distribution: str = "random"
@@ -29,47 +45,36 @@ class Lab:
         match distribution:
             case "random":
                 while len(positions) < init_population_count:
-                    new_pos = (randint(0, self.height - 1), randint(0, self.width - 1))
-                    if new_pos not in positions:
-                        positions.append(new_pos)
+                    new_pos_values = (
+                        randint(0, self.space.height - 1),
+                        randint(0, self.space.width - 1),
+                    )
+                    if new_pos_values not in positions:
+                        positions.append(
+                            Position(
+                                new_pos_values[0], new_pos_values[1], self.space.genesis
+                            )
+                        )
         return [
             Agent(
-                population_map=self.population_map,
-                map_lock=self.map_lock,
+                space=self.space,
                 initial_position=pos,
                 generation=0,
             )
             for pos in positions
         ]
 
-    # VISUALIZATION
-    def get_id_map(self):
-        id_map = np.vectorize(
-            lambda agent: 255 - agent.id % 200 if agent is not None else 0,
-        )(self.population_map)
-        return id_map
-
 
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
 
     # Settings
-    height = 36
-    width = 50
+    height = 72
+    width = 100
     init_pop_count = 10
 
     # Init lab
     lab = Lab(height=height, width=width, init_population_count=init_pop_count)
-    assert np.sum(lab.population_map != None) == init_pop_count  # TODO add to tests
-
-    for agent in lab.population:
-        agent.start()
-
-    print(f"Active threads: {threading.active_count()-1}")
-    sleep(1)
-    for agent in lab.population:
-        agent.kill()
-    # Visualization
-    id_map = lab.get_id_map()
-    plt.imshow(id_map)
-    plt.show()
+    assert np.sum(lab.space.array != None) == init_pop_count  # TODO move to tests
+    lab.experiment(duration=1e-1)
+    lab.analyze(n_viz=3)
