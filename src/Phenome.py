06537588874c8386326239e3714228fc4adf9e49
Alexis JAPAS <alexis.japas.dev@pm.me>
2024-01-30 01:27:12 +0100

    Copy and color

--- src/Phenome.py
+++ src/Phenome.py
@@ -1,12 +1,5 @@
-from enum import Enum
-
 from .Brain import Brain
-
-
-class Abilities(Enum):
-    idle = "idle"
-    move = "move"
-    reproduce = "reproduce"
+from random import randint
 
 
 class Phenome:
@@ -20,14 +13,27 @@ class Phenome:
         self,
         reaction_time: float = 10**-5,
         speed: int = 1000,
-        energy: int = 1000,
+        energy: int = 100,
         scope: int = 1,
+        color: tuple = None,
+        brain: Brain = None,
     ):
         self.reaction_time: float = reaction_time
         self.speed: int = speed
         self.energy: int = energy
         self.scope: int = scope
-        self.brain: Brain = Brain(possible_actions=Abilities)
+        self.color: tuple = (
+            color if color else (randint(5, 252), randint(5, 252), randint(5, 252))
+        )
+        self.brain: Brain = brain if brain else Brain()
 
-    def mutate(self):  # TODO
-        return Phenome(energy=1000)
+    def copy(self, mutation: float = 0):
+        # TODO mutation
+        return Phenome(
+            reaction_time=self.reaction_time,
+            speed=self.speed,
+            energy=self.energy,
+            scope=self.scope,
+            color=self.color,  # TODO how to mutate it?
+            brain=self.brain.copy(mutation=mutation),
+        )
