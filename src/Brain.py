f22abe5a320e9dbd759cd87262c404cadb7bcf74
Alexis JAPAS <alexis.japas.dev@pm.me>
2024-01-30 01:28:03 +0100

    copy method and weights as attribute

--- src/Brain.py
+++ src/Brain.py
@@ -1,12 +1,21 @@
 import random
+from enum import Enum
+
+
+class Abilities(Enum):
+    idle = "idle"
+    move = "move"
+    reproduce = "reproduce"
 
 
 class Brain:
-    def __init__(self, possible_actions) -> None:
-        self.possible_actions = possible_actions
+    def __init__(self, weights=[0.5, 0.3, 0.2]) -> None:
+        self.weights = weights
 
     def __call__(self, inputs: list):
-        action = random.choices(
-            [pa for pa in self.possible_actions], weights=[0.09, 0.90, 0.01]
-        )
+        action = random.choices([pa for pa in Abilities], self.weights)
         return action[0]
+
+    def copy(self, mutation: float = 0):
+        # TODO mutation
+        return Brain(weights=self.weights)
