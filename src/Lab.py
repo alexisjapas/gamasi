342566bf05314f43794279effa59afb462c10d9e
Alexis JAPAS <alexis.japas.dev@pm.me>
2024-02-01 18:49:53 +0100

    Single lock

--- src/Lab.py
+++ src/Lab.py
@@ -23,19 +23,19 @@ class Lab:
         self.universe = Universe(height=height, width=width)
 
         # Population
-        self.lab_authorization = True  # TODO use Threading event
         self.init_population_count = init_population_count
         self._invoke_population(init_population_count)
         assert np.sum(self.universe.space != None) == init_population_count
 
     def _start_agents(self):
-        with Agent.living_lock:
-            for agent in Agent.living.values():
+        with Agent.lock:  # Lock births
+            for agent in Agent.population.values():
                 agent.start()
+            print("Agents started")
 
     def _stop_agents(self):
-        with Agent.living_lock:
-            for agent in Agent.living.values():
+        with Agent.lock:
+            for agent in Agent.population.values():
                 agent.stop.set()
 
     def _invoke_population(
@@ -59,7 +59,6 @@ class Lab:
                 )
         [
             Agent(
-                lab_authorization=self.lab_authorization,
                 universe=self.universe,
                 initial_position=pos,
                 generation=0,
@@ -73,33 +72,31 @@ class Lab:
         self._start_agents()  # TODO all agents shall wait until starting
 
         # Run
-        while duration > 0 and len(Agent.living) > 0:
-            sleep(1)
-            duration -= 1
+        while duration > 0:  # TODO Stop when all died?
+            sleep(0.1)
+            duration -= 0.1
 
         # Stop
-        self.lab_authorization=False
+        self.universe.freeze = True
         self._stop_agents()
 
-    def analyze(self, n_viz=4):
-        n_viz = min(n_viz, len(Agent.living) + len(Agent.dead))
+        # return TODO the results of the experiment in order to run multiple experiments
+
+    def analyze(self, n_viz=4):  # TODO Copy agents until analyse
+        # TODO  Add an argument of data (agents...) to analyze or analyze last one
+        n_viz = min(n_viz, Agent.count)
 
         # Some stats
-        print(f"Living: {len(Agent.living)} | Dead: {len(Agent.dead)}")
-        paths_lengths = [
-            len(agent.path)
-            for agent in list(Agent.living.values()) + list(Agent.dead.values())
-        ]
+        print(f"Total agents: {Agent.count}")
+        paths_lengths = [len(agent.path) for agent in Agent.population.values()]
         paths_lengths.sort()
         path_len_mean = int(sum(paths_lengths) / len(paths_lengths))
         print(f"Agents mean path len = {path_len_mean} px")
         path_len_median = paths_lengths[len(paths_lengths) // 2]
         print(f"Agents median path len = {path_len_median} px")
 
-        # Display paths of some dead and living agents
-        agents = list(
-            [a for a in Agent.living.values()] + [a for a in Agent.dead.values()]
-        )
+        # Display paths of some agents
+        agents = list([a for a in Agent.population.values()])
         fig = plt.figure()
         n_rows = ceil(n_viz ** (1 / 2))
         n_cols = ceil(n_viz / n_rows)
@@ -110,21 +107,23 @@ class Lab:
             plt.axis("off")
 
     def generate_actions_timeline(self, time_step):
-        # TODO it does not render things after the last action,
         # TODO maybe use copy()
         # TODO look for a method to determine optimal time_step
-        actives: list = [a for a in Agent.living.values() if a.path] + [
-            a for a in Agent.dead.values() if a.path
-        ]
-        inactives: list = [a for a in Agent.living.values() if not a.path] + [
-            a for a in Agent.dead.values() if not a.path
-        ]
+        actives: list = [a for a in Agent.population.values() if a.path]
+        inactives: list = [a for a in Agent.population.values() if not a.path]
         time = min([a.path[0].t for a in actives])
         self.universe.init_space()  # Reset universe space
 
         while actives:
+            # Removing deads
+            actives: list = [
+                a for a in actives if a.death_date is None or time < a.death_date
+            ]
+            inactives: list = [
+                a for a in inactives if a.death_date is None or time < a.death_date
+            ]
             # Update time and position of active agents
-            time += time_step
+            
             for agent in [a for a in actives if a.path[0].t <= time]:
                 i = 0
                 while agent.path and agent.path[0].t <= time:
@@ -137,20 +136,16 @@ class Lab:
                     inactives.append(agent)
 
             # Display agents
-            frame = np.zeros(
+            frame = np.ones(
                 (self.universe.space.shape[0], self.universe.space.shape[1], 3),
                 dtype=np.uint8,
             )
             for agent in actives + inactives:
                 if agent.position.t <= time:
                     frame[agent.position.y, agent.position.x] = agent.phenome.color
-            # Removing deads
-            actives: list = [
-                a for a in actives if a.death_date is None or time < a.death_date
-            ]
-            inactives: list = [
-                a for a in inactives if a.death_date is None or time < a.death_date
-            ]
+            
+
+            time += time_step
 
             # Yield
             yield frame
