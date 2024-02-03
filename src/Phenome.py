ce1d6302dff0cb3f4f8bbe8f2731d59e324a4139
Alexis JAPAS <alexis.japas.dev@pm.me>
2024-02-03 17:14:23 +0100

    Copy

--- src/Phenome.py
+++ src/Phenome.py
@@ -1,20 +1,15 @@
-from .Brain import Brain
 from random import randint
+
+from .Brain import Brain
 
 
 class Phenome:
-    """
-    It will be possible to create one phenome from other phenome(s) with mutations
-    Hereditary attributes are locked into a structure to be passed on to the next generation
-    But they are copied and can evolve during the life of the agent
-    """
-
     def __init__(
         self,
         reaction_time: float = 1e-5,
         speed: int = 1e5,
-        energy_capacity: int = 1e4,
-        scope: int = 1,
+        energy_capacity: int = 1e2,
+        scope: int = 3,
         color: tuple = None,
         brain: Brain = None,
     ):
@@ -27,13 +22,16 @@ class Phenome:
         )
         self.brain: Brain = brain if brain else Brain()
 
-    def copy(self, mutation: float = 0):
-        # TODO mutation
+    def copy(self):
         return Phenome(
             reaction_time=self.reaction_time,
             speed=self.speed,
             energy_capacity=self.energy_capacity,
             scope=self.scope,
-            color=self.color,  # TODO how to mutate it?
-            brain=self.brain.copy(mutation=mutation),
+            color=self.color,
+            brain=self.brain.copy(),
         )
+
+    def mutate(self):
+        # TODO
+        pass
